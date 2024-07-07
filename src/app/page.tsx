6fb18e84cdab1733f35e5e2304bce594c3ac3cd6
Hatimnofear <hatimnofear@gmail.com>
2024-07-08 03:52:10 +0500

    work01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import { useState } from "react";
 import Connect from "./components/Connect";
+import UploadContract from "./components/UploadContract";
+
 
 export default function Home() {
   const [signer, setSigner] = useState(undefined);
@@ -14,7 +16,7 @@ export default function Home() {
       <h1 id="title" className="fw-bold mt-2">Universal BlockChain App</h1>
       <p id="sub-title" className="mt-4 fw-bold"><span>A FrontEnd interface <br /> for Any Smart Contract </span></p>
       </div>
-      {signer ? null : <Connect setSigner = {setSigner}/> }
+      {signer ? <UploadContract /> : <Connect setSigner = {setSigner}/> }
       </div>
     </div>
     </main>
